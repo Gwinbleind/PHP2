0e7a18f60aea7138ceec2398f5d7039fedc7ca79
Gwinbleind <borislav31@ya.ru>
2020-07-06 17:12:31 +0300

    test .htaccess

--- views/layouts/layout.php
+++ views/layouts/layout.php
@@ -12,7 +12,7 @@
 	<title>Document</title>
 	<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 	<script src="js/shop.js" async=""></script>
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="/css/style.css">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700&amp;display=swap">
 	<link rel="stylesheet" href="https://use.fontawesome.com/7f8eaeebe5.css">
 </head>
